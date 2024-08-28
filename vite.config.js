511c524aa3cc8af241d22aea6d0b5e8e53cfb13d
andres.arango <andres.arango@blankfactor.com>
2024-08-28 09:04:07 -0500

    port

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
     server: {
-        // hmr: {
-        //     host: "0.0.0.0",
-        // },
+        hmr: {
+            host: "0.0.0.0",
+            port: process.env.PORT || 3000,
+        },
         port: process.env.PORT || 3000, // Default to 3000 if PORT is not set
         host: true,
     },
